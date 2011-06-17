3e0a6e4c68812aa69083cbc676738517a0283f8b
Ryan Fitzgerald <ryan@gymlogr.com>
2011-06-17 15:11:41 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsonIssue::Application.config.secret_token = 'b4aff496d9248e3adc51e8d823cfe93e7244c9cca3f571d443d1cd64c1fa33dcdd3f923033eca8a96a77723b4793d06b92d22d155e3f72d35760526cb3a94b06'
